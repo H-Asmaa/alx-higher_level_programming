eda601f282562a8bbfea78d53eadac6cbf72ff64
H-Asmaa <asmaehadar32@gmail.com>
2023-06-06 17:38:36 +0100

    Printing alphabes accept q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-    if (chr(i) != 'q'  or chr(i) != 'e'):
+    if (chr(i) != 'q' and chr(i) != 'e'):
         print("{}".format(chr(i)), end='')
