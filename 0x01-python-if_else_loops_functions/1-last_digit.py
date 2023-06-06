32890075756f54eaf7d152403042877f7c43945e
H-Asmaa <asmaehadar32@gmail.com>
2023-06-06 17:06:19 +0100

    Greater and less than

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
 if last > 5 and number > 0:
-    str = "is greater than 5"
+    str = "is greater than 5 and not 0"
 elif last > 5 and last > 0 and number < 0:
-    str = "is less than 6"
+    str = "is less than 6 and not 0"
     last *= -1
 elif last < 6 and last > 0:
-    str = "is less than 6"
+    str = "is less than 6 and not 0"
 elif last < 6 and last > 0 and number < 0:
-    str = "is less than 6"
+    str = "is less than 6 and not 0"
     last *= -1
 else:
     str = "is 0"
