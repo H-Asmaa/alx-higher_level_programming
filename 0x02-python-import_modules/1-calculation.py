c4a7cd38b5471eeb5b690d500327779635b4468b
H-Asmaa <asmaehadar32@gmail.com>
2023-06-08 05:20:10 +0100

    My first toolbox

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as c
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, c.add(a, b)))
+print("{} + {} = {}".format(a, b, c.sub(a, b)))
+print("{} + {} = {}".format(a, b, c.mul(a, b)))
+print("{} + {} = {}".format(a, b, c.div(a, b)))
