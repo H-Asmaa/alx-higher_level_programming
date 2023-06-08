aa41f510be21cf5fb58a1d0513b4ca2723c81427
H-Asmaa <asmaehadar32@gmail.com>
2023-06-08 16:56:57 +0100

    program that imports functions from the file calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import calculator_1 as c
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, c.add(a, b)))
-print("{} + {} = {}".format(a, b, c.sub(a, b)))
-print("{} + {} = {}".format(a, b, c.mul(a, b)))
-print("{} + {} = {}".format(a, b, c.div(a, b)))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, c.add(a, b)))
+    print("{} + {} = {}".format(a, b, c.sub(a, b)))
+    print("{} + {} = {}".format(a, b, c.mul(a, b)))
+    print("{} + {} = {}".format(a, b, c.div(a, b)))
