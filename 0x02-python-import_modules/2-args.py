247a462e5a2eb6bc28b52c41dede783dee1ffe47
H-Asmaa <asmaehadar32@gmail.com>
2023-06-08 17:21:34 +0100

    program that imports functions from the file calculator_1.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,8 @@ def main():
     print("{} argument".format(len(argv) - 1), end="")
     if len(argv) == 1:
         print("s.")
+    elif len(argv) == 2:
+        print(":")
     else:
         print("s:")
     for i in range(1, len(argv)):
@@ -15,4 +17,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
