fdf2c7c287234094e883e45fc939c9b8db777a19
H-Asmaa <asmaehadar32@gmail.com>
2023-06-13 01:19:36 +0100

    A function that returns the max value in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if not my_list:
+        return None
     sorted_list = my_list.copy()
     for i in range(len(sorted_list)):
         for j in range(0, len(sorted_list) - i - 1):
