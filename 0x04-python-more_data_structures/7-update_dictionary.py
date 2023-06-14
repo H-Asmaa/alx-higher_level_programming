6725e6218c90f991fed40377fc7488318e552c21
H-Asmaa <asmaehadar32@gmail.com>
2023-06-14 22:34:02 +0100

    Updating a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for key_search in a_dictionary:
+        if key_search == key:
+            a_dictionary[key] = value
+    return (a_dictionary)
