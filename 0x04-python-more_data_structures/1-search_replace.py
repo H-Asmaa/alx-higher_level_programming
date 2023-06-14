b7e287ad4e33a4f802bc1c608735fdcb215a6208
H-Asmaa <asmaehadar32@gmail.com>
2023-06-14 19:35:45 +0100

    Replacing all acurrences of an element in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,6 @@
 def search_replace(my_list, search, replace):
     my_list_copy = my_list.copy()
     for i in range(len(my_list_copy)):
-        if 
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+        if my_list_copy[i] == search:
+            my_list_copy[i] = replace
+    return my_list_copy
