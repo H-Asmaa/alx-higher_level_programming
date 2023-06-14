d6f6c5b4ee8bffe8047e4e997356cf337842875c
H-Asmaa <asmaehadar32@gmail.com>
2023-06-14 19:28:55 +0100

    Square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_copy = [row[:] for row in matrix]
-    for i in range(len(matrix_copy)):
-        for j in range(len(matrix_copy[i])):
-            matrix_copy[i][j] = matrix_copy[i][j] ** 2
-    return matrix_copy
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    my_list_copy = my_list.copy()
+    for i in range(len(my_list_copy)):
+        if 
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
