1f4060cb47f1967600b36bf963a06f5f82e38233
H-Asmaa <asmaehadar32@gmail.com>
2023-06-14 22:34:49 +0100

    Updating a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,3 +4,6 @@ def update_dictionary(a_dictionary, key, value):
         if key_search == key:
             a_dictionary[key] = value
     return (a_dictionary)
+def print_sorted_dictionary(a_dictionary):
+    for key, val in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, val))
