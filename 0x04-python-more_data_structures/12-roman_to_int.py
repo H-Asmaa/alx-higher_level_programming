4e0e091cc943504141ce0a54fc4cf0943ef9bd1b
H-Asmaa <asmaehadar32@gmail.com>
2023-06-15 01:14:33 +0100

    Roman numbers translator

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return None
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     roman_val = {
         'I': 1,
         'V': 5,
