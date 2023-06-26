009f1d6bd1af5f4b8600f8251f642e27885a1e64
H-Asmaa <asmaehadar32@gmail.com>
2023-06-26 19:23:44 +0100

    Exception handeling in the case of value error

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return (True)
+        else:
+            return (False)
+    except ValueError:
+        pass
