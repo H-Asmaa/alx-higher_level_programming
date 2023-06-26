e51358dfdce652d6bc2d0927da19e03b1459d15b
H-Asmaa <asmaehadar32@gmail.com>
2023-06-27 00:09:07 +0100

    The innovation of a friend

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,8 +2,6 @@
 def safe_print_list_integers(my_list=[], x=0):
     L = []
     count = 0
-    if my_list is None:
-        return 0
     for i in range(x):
         try:
             if isinstance(my_list[i], int):
