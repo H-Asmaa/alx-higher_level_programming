3c1191f030a83bbcad24070439650ebf32861c70
H-Asmaa <asmaehadar32@gmail.com>
2023-06-27 18:26:23 +0100

    Using exceptions inside of the init method of the class Square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,36 +17,3 @@ class Square:
             self.__size = size
         else:
             raise TypeError("size must be an integer")
-
-
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
-
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
