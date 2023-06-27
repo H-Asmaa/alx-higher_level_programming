bce00a7dff4e013340c4109a2310ef4d3ad7f90a
H-Asmaa <asmaehadar32@gmail.com>
2023-06-27 16:45:21 +0100

    an empty class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
