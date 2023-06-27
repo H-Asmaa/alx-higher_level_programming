1aebd7cdaf87da5207014debc24914fc94ef044f
H-Asmaa <asmaehadar32@gmail.com>
2023-06-27 18:14:46 +0100

    Using exceptions inside of the init method of the class Square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Completing the class Square"""
+
+
+class Square:
+    """Class Square with Private instance attribute: size
+    The method __init__ contains exceptions for when size
+    is not an int and when it's less than zero.
+    The initialisation of size to zero will serve it's pu
+    -rpose when the size is not given"""
+    __size = None
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
