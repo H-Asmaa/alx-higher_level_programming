77d90120d5c1fc7679a32a6f4700468b09435ab5
H-Asmaa <asmaehadar32@gmail.com>
2023-07-03 20:55:07 +0100

    Committing rectangle class with attributes and methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,14 +3,31 @@
 
 
 class Rectangle:
-    """The class Rectangle"""
+    """The class Rectangle
+    Private instance attribute: width:
+        - property def width(self): to retrieve it
+        - property setter def width(self, value): to set it:
+                        # width must be an integer, otherwise raise a TypeError
+                        exception with the message width must be an integer
+                        # if width is less than 0, raise a ValueError exception
+                with the message width must be >= 0
+
+        Private instance attribute: height:
+                same thing goes exactly as width.
+        Instantiation with optional width and height:
+        def __init__(self, width=0, height=0):
+    """
     __width = None
     __height = None
 
     def __init__(self, width=0, height=0):
-        """__init__ dunder"""
-        self.__height = height
+        """__init__ dunder
+        Arguments:
+            width : the width of rectangle
+            height : the height of rectangle
+        """
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
