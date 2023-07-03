e76ab6fc51a659ab962248f84e711b01c419df20
H-Asmaa <asmaehadar32@gmail.com>
2023-07-03 20:04:19 +0100

    Committing rectangle class with attributes and methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,22 +28,22 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    @property
-    def height(self):
-        return self.__width
-
     @width.setter
     def width(self, value):
-        if isinstance(value, int):
+        if not isinstance(value, int):
             self.__width = value
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             raise TypeError("width must be an integer")
 
+    @property
+    def height(self):
+        return self.__width
+
     @height.setter
     def height(self, value):
-        if isinstance(value, int):
+        if not isinstance(value, int):
             self.__height = value
         elif value < 0:
             raise ValueError("height must be >= 0")
