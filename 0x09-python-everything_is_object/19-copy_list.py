f1daecc45b2207fc135a93de67690199f5bab3aa
H-Asmaa <asmaehadar32@gmail.com>
2023-07-04 17:55:08 +0100

    Committing the update for the copy function, adding the last line

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
 def copy_list(list):
     new_list = list[:]
-    return my_list
-
-my_list = [1, 2, 3]
-print(my_list)
-
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
+    return new_list
