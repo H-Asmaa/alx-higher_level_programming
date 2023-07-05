76b01febce14326a3e74d0bdec1b47e15d9d5364
H-Asmaa <asmaehadar32@gmail.com>
2023-07-05 01:05:28 +0100

    Committing class LockedClass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,5 +4,6 @@
 
 class LockedClass:
     """A class that prevents the user from dynamically creating new instance
-    attributes, except if the new instance attribute is called first_name."""
+    attributes, except if the new instance attribute is called first_name.
+    __slots__ : attribute altering"""
     __slots__ = ['first_name']
