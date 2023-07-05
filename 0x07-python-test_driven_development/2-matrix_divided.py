8d649598bb4ae62b8b26b0308aab12f5e35fffc5
H-Asmaa <asmaehadar32@gmail.com>
2023-07-05 11:08:42 +0100

    Committing an extra test for checking

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """
+    Function that performs addition of two numbers.
+
+    Returns :
+        b casted to int in case a in None
+        a added to b casted to int
+
+    Raises :
+        TypeError in case a is not an int nor a float
+        TypeError in case b is not an int nor a float
+    """
