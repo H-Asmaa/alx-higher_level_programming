b676d86753cd794bb55f6ceecb59be103ea4fab7
H-Asmaa <asmaehadar32@gmail.com>
2023-07-07 16:31:55 +0100

    Committing type test trial

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,9 +14,9 @@ def add_integer(a, b=98):
         TypeError in case a is not an int nor a float
         TypeError in case b is not an int nor a float
     """
-    if a is None or not isinstance(a, (int, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
