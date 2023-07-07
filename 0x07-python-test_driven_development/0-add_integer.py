b8d06a35e27c646fcf43733abb26767fee199e1b
H-Asmaa <asmaehadar32@gmail.com>
2023-07-07 14:58:05 +0100

    Committing the condition if a is not None

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,6 +16,8 @@ def add_integer(a, b=98):
     """
     if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+    if isinstance(b, float) and b != b:
+        raise TypeError("b must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
