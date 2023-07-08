91bce5b72cd55a3d9e61400db3bf04cfdb110f6d
H-Asmaa <asmaehadar32@gmail.com>
2023-07-08 03:50:28 +0100

    Committing trial test

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lazy_matrix function"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Function that prints a text with 2 new lines after each of
+    these characters: ., ? and :.
+
+    Returns :
+        Nothing
+
+    Raises :
+        TypeError in case the text is not string
+    """
+    
