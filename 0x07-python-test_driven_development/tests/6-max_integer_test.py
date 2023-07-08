052452adce7941c605cff0e4170e36a5069eee70
H-Asmaa <asmaehadar32@gmail.com>
2023-07-08 03:11:56 +0100

    Committing methods saperated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,23 +3,10 @@ import unittest
 """Module to find the max integer in a list
 """
 
-
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
-
+max_integer = __import__('6-max_integer').max_integer
 
 class Test_method(unittest.TestCase):
+    """unittest class"""
     def test_max_end(self):
         test = max_integer([1, 4, 8, 10])
         self.assertEqual(test, 10)
