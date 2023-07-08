2983cbd0c581c13f84d050524b35d43ba3f68c3e
H-Asmaa <asmaehadar32@gmail.com>
2023-07-08 03:06:53 +0100

    Committing methods saperated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -20,21 +20,30 @@ def max_integer(list=[]):
 
 
 class Test_method(unittest.TestCase):
-    def test_max_integer(self):
+    def test_max_end(self):
         test = max_integer([1, 4, 8, 10])
         self.assertEqual(test, 10)
-        test = max_integer([1, 4, -8, 100])
-        self.assertEqual(test, 100)
-        test = max_integer([])
-        self.assertEqual(test, None)
+
+    def test_max_begin(self):
         test = max_integer([333, 4, -8, 100])
         self.assertEqual(test, 333)
+
+    def test_max_middle(self):
         test = max_integer([333, 4, 800, 100])
         self.assertEqual(test, 800)
+
+    def test_max_negative(self):
         test = max_integer([-333, -4, -800, -100])
         self.assertEqual(test, -4)
+
+    def test_ampty(self):
+        test = max_integer([])
+        self.assertEqual(test, None)
+
+    def test_one_elm(self):
         test = max_integer([1])
         self.assertEqual(test, 1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
