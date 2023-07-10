eec49cac84ae03e59c0d6f174a5008c7c9c5836e
H-Asmaa <asmaehadar32@gmail.com>
2023-07-11 00:31:52 +0100

    Committing Main fix

--- 0x0A-python-inheritance/MAIN/100-main.py
+++ 0x0A-python-inheritance/MAIN/100-main.py
@@ -6,9 +6,8 @@ lookup_path = './100-my_int.py'
 spec = importlib.util.spec_from_file_location('MyInt', lookup_path)
 lookup_module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(lookup_module)
-my_i = lookup_module.MyInt
 
-my_i = MyInt(3)
+my_i = lookup_module.MyInt(3)
 print(my_i)
 print(my_i == 3)
 print(my_i != 3)
