0a1e0e115107c7ad6ca0c958e0dd919b980e3777
H-Asmaa <asmaehadar32@gmail.com>
2023-07-10 20:16:16 +0100

    Committing isinstance check

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,8 +5,8 @@
 
 
 def is_same_class(obj, a_class):
-    """ a function that returns True if the object is exactly an
-    instance of the specified class ; otherwise False."""
-    if not isinstance(obj, a_class):
-        return False
-    return True
+    """ a function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False."""
+    if type(obj) is a_class:
+        return True
+    return False
