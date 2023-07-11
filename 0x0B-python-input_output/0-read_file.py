07664a0763d5540f7dbe7e9aa7ab1232b62538cb
H-Asmaa <asmaehadar32@gmail.com>
2023-07-11 16:33:51 +0100

    Committing change of code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 
 def read_file(filename=""):
     """A function that reads a text file (UTF8) and prints it to stdout."""
-    with open('my_file_0.txt', 'r', encoding="UTF-8") as f:
-        read = f.read()
-    print(read)
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
