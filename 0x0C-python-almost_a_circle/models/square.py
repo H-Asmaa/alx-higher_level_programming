8265d536d1d5dc394a43845fe72302ae25659de5
H-Asmaa <asmaehadar32@gmail.com>
2023-07-17 13:26:22 +0100

    Committing new subclass Square that inherits from Rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+0x0C. Python - Almost a circle
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square that inherits from Rectangle:
+        Task 10:
+            Class constructor:
+            def __init__(self, size, x=0, y=0, id=None):
+                - Call the super class with id, x, y, width and
+                height, this super call will use the logic of the
+                __init__ of the
+                Rectangle class. The width and height must be
+                assigned to the value of size.
+                - You must not create new attributes for this class,
+                use all attributes of Rectangle - As reminder: a Square
+                is a Rectangle with the same width and height
+                - All width, height, x and y validation must inherit from
+                Rectangle - same behavior in case of wrong data
+            The overloading __str__ method should return [Square] (<id>)
+            <x>/<y> - <size> - in our case, width or height
+            A Square is a special Rectangle, so it makes sense this class
+            Square inherits from Rectangle. Now you have a Square class
+            who has the same attributes and same methods.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id=None)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
