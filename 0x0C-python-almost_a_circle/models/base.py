d7b4aaaa7f7099ea3fc07134ac891c5967ab84e3
H-Asmaa <asmaehadar32@gmail.com>
2023-07-17 16:24:36 +0100

    Committing JSON string to file, that transforms an obj to JSON str and saves it to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,14 @@ class Base:
                 - list_dictionaries is a list of dictionaries
                 - If list_dictionaries is None or empty, return "[]"
                 - Otherwise, return the JSON string representations
+        Task 16:
+            Adding the class method def save_to_file(cls, list_objs): that
+            writes the JSON string representation of list_objs to a file:
+                - list_objs is a list of instances who inherits of Base
+                - If list_objs is None, save an empty list
+                - The filename must be: <Class name>.json ex: Rectangle.json
+                - You must use the static method to_json_string
+                - You must overwrite the file if it already exists
     """
     __nb_objects = 0
 
@@ -35,7 +43,20 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        file_name = cls.__name__ + ".json"
+        list_ = []
+        with open(file_name, "w", encoding="UTF-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                for i in list_objs:
+                    list_.append(i.to_dictionary())
+                f.write(Base.to_json_string(list_))
