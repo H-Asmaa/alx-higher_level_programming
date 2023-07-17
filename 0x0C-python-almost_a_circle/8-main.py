98a8a643df9f9a62a206417fed628b1729f4eb5d
H-Asmaa <asmaehadar32@gmail.com>
2023-07-17 13:27:48 +0100

    Committing pycodestyle correction for 8-main.py

--- 0x0C-python-almost_a_circle/8-main.py
+++ 0x0C-python-almost_a_circle/8-main.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     r1 = Rectangle(10, 10, 10, 10)
     print(r1)
     """
-    	In this specific test case, r1 = Rectangle(10, 10, 10, 10)
-        the id attribute is assigned the value of 1 because it is
-        the first instance of the Rectangle class created in your
-        code. The __nb_objects counter is incremented when this
-        instance is created, and the incremented value is assigned
-        to the id attribute.
+    In this specific test case, r1 = Rectangle(10, 10, 10, 10)
+    the id attribute is assigned the value of 1 because it is
+    the first instance of the Rectangle class created in your
+    code. The __nb_objects counter is incremented when this
+    instance is created, and the incremented value is assigned
+    to the id attribute.
     """
     r1.update(89)
     print(r1)
