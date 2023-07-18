bfb898edd2d50039dfec0bba3ae33b37ae2aeaaa
H-Asmaa <asmaehadar32@gmail.com>
2023-07-18 07:49:40 +0100

    Committing the id fix for square, it was taking None

--- 0x0C-python-almost_a_circle/10-main.py
+++ 0x0C-python-almost_a_circle/10-main.py
@@ -22,3 +22,31 @@ if __name__ == "__main__":
     print(s3)
     print(s3.area())
     s3.display()
+
+    """
+        Main from QA.
+        Expected output = OK
+        Got = OK
+    """
+    s = Square(5, 7, 2, 89)
+    if s.id != 89:
+        print("ID must be equal to 89: {}".format(s.id))
+        exit(1)
+
+    if s.width != 5:
+        print("Width of the Square must be 5: {}".format(s.width))
+        exit(1)
+
+    if s.height != 5:
+        print("Height of the Square must be 5: {}".format(s.height))
+        exit(1)
+
+    if s.x != 7:
+        print("X of the Square must be 7: {}".format(s.x))
+        exit(1)
+
+    if s.y != 2:
+        print("Y of the Square must be 2: {}".format(s.y))
+        exit(1)
+
+    print("OK", end="")
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -59,7 +59,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """__init__ method"""
-        super().__init__(size, size, x, y, id=None)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
