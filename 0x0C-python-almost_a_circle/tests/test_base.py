a857b10a0a11cd04b9083a0bd0af4d8fb3e2d8d5
H-Asmaa <asmaehadar32@gmail.com>
2023-07-18 18:15:04 +0100

    Committing Test_base

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,22 +1,50 @@
 """
 	0x0C. Python - Almost a circle
 """
-import os
-import sys
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
-import unittest
 from models.base import Base
+import unittest
+import os
 
 
 class TestBase(unittest.TestCase):
     def test_id(self):
         obj_base = Base()
         self.assertIsInstance(obj_base, Base)
-    """
+        obj_base.id = 2
+        self.assertEqual(obj_base.id, 2)
+        obj_base.id = 3
+        self.assertEqual(obj_base.id, 3)
+
     def test_to_json_string(self):
+        obj_base = Base()
+        json_str = obj_base.to_json_string({'x': 2,
+                                            'width': 10,
+                                            'id': 1,
+                                            'height': 7,
+                                            'y': 8})
+        expected_str = '{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}'
+        self.assertIsInstance(json_str, str)
+        self.assertEqual(json_str, expected_str)
+        json_str = obj_base.to_json_string(None)
+        self.assertIsInstance(json_str, str)
+
+
+"""
     def test_save_to_file(self):
+        obj_base = Base()
+        obj_base.save_to_file(Base, {'x': 2,
+                                     'width': 10,
+                                     'id': 1,
+                                     'height': 7,
+                                     'y': 8})
+        with open("Base.json", "r") as file:
+            file_content = file.read()
+        self.assertEqual(file_content, {'x': 2,
+                                        'width': 10,
+                                        'id': 1,
+                                        'height': 7,
+                                        'y': 8})
+
     def test_from_json_string(self):
     def test_create(self):
     def test_load_from_file(self):
