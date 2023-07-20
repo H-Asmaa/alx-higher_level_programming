4f538774c18e5a4707af1c062ebcd6e8d3168a0f
H-Asmaa <asmaehadar32@gmail.com>
2023-07-20 13:12:29 +0100

    Committing the new test for the method create

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,5 +1,5 @@
 """
-	0x0C. Python - Almost a circle
+0x0C. Python - Almost a circle
 """
 from models.base import Base
 from models.rectangle import Rectangle
@@ -8,7 +8,14 @@ import os
 
 
 class TestBase(unittest.TestCase):
+    """
+        All your files, classes and methods must be unit tested
+        and be PEP 8 validated.
+        command : python3 -m unittest discover tests
+    """
+
     def test_id(self):
+        """Testing the attribute ID"""
         obj_base = Base()
         self.assertIsInstance(obj_base, Base)
         obj_base.id = 2
@@ -17,6 +24,7 @@ class TestBase(unittest.TestCase):
         self.assertEqual(obj_base.id, 3)
 
     def test_to_json_string(self):
+        """Testing the method to_json_string"""
         obj_base = Base()
         json_str = obj_base.to_json_string({'x': 2,
                                             'width': 10,
@@ -30,6 +38,7 @@ class TestBase(unittest.TestCase):
         self.assertIsInstance(json_str, str)
 
     def test_from_json_string(self):
+        """Testing the method from_json_string"""
         list_input = [{'height': 2, 'width': 1, 'id': 77},
                       {'height': 18, 'width': 11, 'id': 7}]
         json_str = Rectangle.to_json_string(list_input)
@@ -37,36 +46,12 @@ class TestBase(unittest.TestCase):
         self.assertEqual(list_output, list_input)
         self.assertIsInstance(list_output, list)
 
-
-"""
-    def test_from_json_string(self):
-        list_input = [{'height': 2, 'width': 1, 'id': 77},
-                      {'height': 18, 'width': 11, 'id': 7}]
-        json_str = Rectangle.to_json_string(list_input)
-        list_output = Rectangle.from_json_string(json_str)
-        self.assertEqual(list_output, [{'height': 4, 'width': 10, 'id': 89,
-                                        'x': 3, 'y': 2},
-                                       {'height': 7, 'width': 1, 'id': 7,
-                                        'x': 3}])
-        self.assertIsInstance(list_output, list)
-    def test_save_to_file(self):
-        obj_base = Base()
-        obj_base.save_to_file(Base, {'x': 2,
-                                     'width': 10,
-                                     'id': 1,
-                                     'height': 7,
-                                     'y': 8})
-        with open("Base.json", "r") as file:
-            file_content = file.read()
-        self.assertEqual(file_content, {'x': 2,
-                                        'width': 10,
-                                        'id': 1,
-                                        'height': 7,
-                                        'y': 8})
-
     def test_create(self):
-    def test_load_from_file(self):
-	"""
+        """Testing the method create"""
+        obj = Rectangle(3, 5, 1)
+        obj_dict = obj.to_dictionary()
+        obj_creation = Rectangle.create(**obj_dict)
+        self.assertEqual("[Rectangle] (1) 1/0 - 3/5", str(obj_creation))
 
 
 if __name__ == "__main__":
