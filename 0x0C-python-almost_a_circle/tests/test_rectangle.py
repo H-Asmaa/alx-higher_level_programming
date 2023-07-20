76c8fb016edaf8cbb2ea8ff8ddc39e7d8393fce0
H-Asmaa <asmaehadar32@gmail.com>
2023-07-20 16:10:55 +0100

    Committing two new tests for __str__ and display

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -5,6 +5,9 @@ from models.base import Base
 from models.rectangle import Rectangle
 import unittest
 import os
+from io import StringIO
+import sys
+from contextlib import redirect_stdout
 
 
 class TestRectangle(unittest.TestCase):
@@ -17,8 +20,30 @@ class TestRectangle(unittest.TestCase):
     def test_area(self):
         """Testing the method area"""
         obj = Rectangle(3, 2)
+        self.assertIsInstance(obj, Rectangle)
         obj_area = obj.area()
         self.assertEqual(6, obj_area)
         obj = Rectangle(6, 2)
         obj_area = obj.area()
         self.assertEqual(12, obj_area)
+
+    def test_display(self):
+        """Testing the method display"""
+        obj = Rectangle(4, 3, 2, 1)
+        output_buffer = StringIO()
+        expected_output = "####\n  ####\n  ####"
+        with redirect_stdout(output_buffer):
+            obj.display()
+        printed_output = output_buffer.getvalue().strip()
+        self.assertEqual(printed_output, expected_output)
+
+    def test___str__(self):
+        """Testing the method __str__"""
+        obj = Rectangle(4, 6, 2, 1, 12)
+        output = obj.__str__()
+        expected_output = "[Rectangle] (12) 2/1 - 4/6"
+        self.assertEqual(output, expected_output)
+        obj_2 = Rectangle(5, 5, 1)
+        output = obj_2.__str__()
+        expected_output = "[Rectangle] (4) 1/0 - 5/5"
+        self.assertEqual(output, expected_output)
