07256e3a8faf817905a54bb2cb0b4d4e11306ea8
H-Asmaa <asmaehadar32@gmail.com>
2023-07-21 12:18:11 +0100

    Committing new testing method for attributes testing and validation

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -13,6 +13,47 @@ class TestSquare(unittest.TestCase):
         command : python3 -m unittest discover tests
     """
 
+    def test_attributes(self):
+        """Testing attributes"""
+        obj = Square(1)
+        self.assertEqual(obj.size, 1)
+        obj = Square(1, 2)
+        self.assertEqual(obj.size, 1)
+        self.assertEqual(obj.x, 2)
+        obj = Square(1, 2, 3)
+        self.assertEqual(obj.size, 1)
+        self.assertEqual(obj.x, 2)
+        self.assertEqual(obj.y, 3)
+        obj = Square(1, 2, 3, 4)
+        self.assertEqual(obj.id, 4)
+        self.assertEqual(obj.size, 1)
+        self.assertEqual(obj.x, 2)
+        self.assertEqual(obj.y, 3)
+
+    def test_attributes_validation(self):
+        """Testing attributes validation"""
+        """---WIDTH---"""
+        with self.assertRaises(TypeError) as traceback:
+            isinstance = Square("a")
+        self.assertEqual(str(traceback.exception), "width must be an integer")
+        with self.assertRaises(ValueError) as traceback:
+            isinstance = Square(-1)
+        self.assertEqual(str(traceback.exception), "width must be > 0")
+        """---X---"""
+        with self.assertRaises(TypeError) as traceback:
+            isinstance = Square(1, "a")
+        self.assertEqual(str(traceback.exception), "x must be an integer")
+        with self.assertRaises(ValueError) as traceback:
+            isinstance = Square(1, -1)
+        self.assertEqual(str(traceback.exception), "x must be >= 0")
+        """---Y---"""
+        with self.assertRaises(TypeError) as traceback:
+            isinstance = Square(1, 2, "a")
+        self.assertEqual(str(traceback.exception), "y must be an integer")
+        with self.assertRaises(ValueError) as traceback:
+            isinstance = Square(1, 2, -1)
+        self.assertEqual(str(traceback.exception), "y must be >= 0")
+
     def test___str__(self):
         """Testing the method __str__"""
         obj = Square(4, 6, 1, 12)
