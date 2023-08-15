ec5f9249259123bd535fbba3d118aa847f5d9584
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 00:04:23 +0100

    Committing max tempreture for each state

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- A script that displays the max temperature of each state (ordered by State name).
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY State;
