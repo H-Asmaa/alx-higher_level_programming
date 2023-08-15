e4a1eaa4aff99c86559acbadc2454f62d129ecfc
H-Asmaa <asmaehadar32@gmail.com>
2023-08-15 23:45:17 +0100

    Committing highest tempreture grouped by cities

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- A script that displays the top 3 of cities temperature during July and August ordered by temperature (descending).
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN ("August", "July") GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN ("July", "August") GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
