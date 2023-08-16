b194df6a2a3ff5108a4160f228174578a342a299
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 11:09:11 +0100

    Committing a script that lists rows of a table

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- A script that lists all rows of the table first_table from the database hbtn_0c_0.
+-- A script that lists all rows of the table first_table from hbtn_0c_0.
 SELECT * FROM first_table;
