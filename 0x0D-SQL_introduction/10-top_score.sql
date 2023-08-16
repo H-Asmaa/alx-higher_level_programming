cdaf92adb68e5d1b2a7666f1d3bcac5d8ef0b3d4
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 11:23:17 +0100

    Committing a script that lists all records of the table second_table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- A script that lists all records of the table second_table of the database hbtn_0c_0.
+-- A script that lists all records of the table second_table.
 SELECT score, name FROM second_table ORDER BY score DESC;
