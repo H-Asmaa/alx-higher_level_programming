0433ff37ae88a0b06dee792cfacf61048619ecb7
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 11:14:02 +0100

    Committing a script that inserts a new row in a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- A script that inserts a new row in the table first_table.
+-- A script that inserts a new row, in the table first_table.
 INSERT INTO first_table(id, name) VALUES (89, 'Best School');
