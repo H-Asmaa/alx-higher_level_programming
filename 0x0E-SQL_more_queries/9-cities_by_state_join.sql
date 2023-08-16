e98df0d480c15e93804dfff48c4f3dcce3cea2a4
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 22:48:57 +0100

    Committing a script that lists all cities contained in the database hbtn_0d_usa.

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name
+SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
 ON cities.states_id = states.states_id
 ORDER BY id ASC;
