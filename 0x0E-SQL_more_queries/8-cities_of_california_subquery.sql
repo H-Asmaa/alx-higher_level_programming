dced1ea6371f09dd292c5184cfd1969398413b00
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 22:29:17 +0100

    Committing script that creates the database hbtn_0d_usa and the table cities.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,8 @@
 -- A script that creates the database hbtn_0d_usa and the table cities.
-SELECT id, name FROM cities WHERE state_id=(
-	SELECT state_id FROM states WHERE name="California"
-) ORDER BY id DESC;
+SELECT id, name
+FROM cities
+WHERE state_id=(
+	SELECT state_id
+	FROM states
+	WHERE name="California"
+)ORDER BY id DESC;
