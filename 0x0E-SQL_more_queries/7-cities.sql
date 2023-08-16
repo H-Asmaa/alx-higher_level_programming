736c087d2613b24c4a6076eab4b915475812bd82
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 21:55:34 +0100

    Committing a script that creates the database hbtn_0d_usa and the table cities.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- A script that creates the database hbtn_0d_usa and the table cities.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(state_id)
+);
