5a8c628fd049e02ace74b12e40724bd5a41b62c3
H-Asmaa <asmaehadar32@gmail.com>
2023-08-16 11:06:11 +0100

    Committing a script that creates a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- A script that creates a table called first_table in the hbtn_0c_0.
+-- A script that creates a table called first_table in the hbtn_0c_0 database.
 CREATE table IF NOT EXISTS first_table(
 	id  int,
 	name  VARCHAR (256)
