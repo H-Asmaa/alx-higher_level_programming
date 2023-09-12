c9b71845e9610350b203197026ac441064858aea
H-Asmaa <asmaehadar32@gmail.com>
2023-09-12 23:18:58 +0100

    Committing a function that adds args along with the conditions to call that function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+const args = process.argv.slice(2);
+if (!isNaN(args[0]) && !isNaN(args[1])) {
+  console.log(add(parseInt(args[0]), parseInt(args[1])));
+} else {
+  console.log(NaN);
+}
