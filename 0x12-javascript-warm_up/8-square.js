f2eb47859f1fb6f183ad865a9c8710e9a96b1f84
H-Asmaa <asmaehadar32@gmail.com>
2023-09-12 18:40:05 +0100

    Committing a script that prints a squareusing X and a given number in args

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+const args = process.argv.slice(2);
+let row = '';
+if (!isNaN(args[0]) && args[0] >= 0) {
+  for (let i = 0; i < args[0]; i++) {
+    for (let j = 0; j < args[0]; j++) {
+      row += 'X';
+    }
+    console.log(row);
+    row = '';
+  }
+} else if (isNaN(args[0])) {
+  console.log('Missing size');
+}
