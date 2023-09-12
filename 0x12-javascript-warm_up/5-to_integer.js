5348e7aa87454f09e04381167e919b9a70d72d5a
H-Asmaa <asmaehadar32@gmail.com>
2023-09-12 17:48:15 +0100

    Committing to integer script

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args[0]) {
+if (!isNaN(Number(args[0]))) {
   console.log('My number: ' + Number(args[0]));
 } else {
   console.log('Not a number');
