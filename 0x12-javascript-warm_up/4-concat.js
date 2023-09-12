55343c87ab8e59728fa5bf6d82c6ad6ffad263cb
H-Asmaa <asmaehadar32@gmail.com>
2023-09-12 17:31:09 +0100

    Committing concatenation of args

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,9 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args[0] && args[1]) {
+  console.log(args[0] + ' is ' + args[1]);
+} else if (args[0]) {
+  console.log(args[0] + ' is undefined');
+} else {
+  console.log('undefined is undefined');
+}
