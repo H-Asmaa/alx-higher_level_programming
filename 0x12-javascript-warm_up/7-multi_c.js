e566b6974b7020e49236ad48ab03747a2efe6488
H-Asmaa <asmaehadar32@gmail.com>
2023-09-12 18:14:49 +0100

    Committing a script that prints x times “C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (!isNaN(args[0]) && args[0] >= 0) {
+  for (let i = 0; i < args[0]; i++) {
+    console.log('C is fun');
+  }
+} else if (isNaN(args[0])) {
+  console.log('Missing number of occurrences');
+}
