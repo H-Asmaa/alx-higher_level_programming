41c26eb8b022cfbda71a623881c5e276ffa6e733
H-Asmaa <asmaehadar32@gmail.com>
2023-09-13 00:28:33 +0100

    Committing a script that finds the second biggest number in a group of args

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,24 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+function secondBiggest (list) {
+  let first = -1;
+  let second = -1;
+  for (const i of list) {
+    if (first < i) {
+      second = first;
+      first = i;
+    } else if (second < i && first > i) {
+      second = i;
+    }
+  }
+  return second;
+}
+const args = process.argv.slice(2);
+let res = 0;
+if (!args[0]) {
+  console.log(0);
+} else if (args.length === 1) {
+  console.log(0);
+} else {
+  res = secondBiggest(args);
+  console.log(res);
+}
