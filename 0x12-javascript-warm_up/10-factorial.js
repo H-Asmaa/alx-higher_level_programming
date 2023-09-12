7c2e877e1af8c1f1c371956e16ffd4c3e3828b17
H-Asmaa <asmaehadar32@gmail.com>
2023-09-12 23:36:34 +0100

    Committing a function that performs factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+function factorial (n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+const args = process.argv.slice(2);
+let res = 0;
+if (!args[0]) {
+  res = 1;
+  console.log(res);
+} else if (!isNaN(args[0])) {
+  res = factorial(parseInt(args[0]));
+  console.log(res);
+}
