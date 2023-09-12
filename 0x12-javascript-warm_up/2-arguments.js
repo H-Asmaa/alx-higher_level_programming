59a112f2bdbe045689f67aa35884574a0ecfe6ba
H-Asmaa <asmaehadar32@gmail.com>
2023-09-12 15:15:44 +0100

    Committing handeling args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args.length < 1) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
