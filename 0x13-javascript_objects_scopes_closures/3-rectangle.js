14a6c20ecedd8557041d79a007cc6c59b08e8718
H-Asmaa <asmaehadar32@gmail.com>
2023-09-13 23:44:19 +0100

    Committing the rectangle class with an addition of a print instance method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+      row = '';
+    }
+  }
+}
+module.exports = Rectangle;
