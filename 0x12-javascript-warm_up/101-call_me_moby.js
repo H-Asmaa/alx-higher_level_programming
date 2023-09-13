baf4dccf43c6fc42d4a804368e43d93777d4e4ce
H-Asmaa <asmaehadar32@gmail.com>
2023-09-13 19:12:55 +0100

    Committing a change of a variable from a different file

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,10 @@
 #!/usr/bin/no
-function callMeMoby(x, theFunction) {
-    if (x > 0) {
-        callMeMoby(x - 1, theFunction);
-        theFunction();
-    }
+function callMeMoby (x, theFunction) {
+  if (x > 0) {
+    callMeMoby(x - 1, theFunction);
+    theFunction();
+  }
 }
 module.exports = {
-    callMeMoby
+  callMeMoby
 };
