4b4331c11de561b1245374d7cd6240c1581e0047
H-Asmaa <asmaehadar32@gmail.com>
2023-09-14 08:28:21 +0100

    Committing a function that returns the number of occurences of an element

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/no
+exports.nbOccurences = function (list, searchElement) {
+  let occurrences = 0;
+  for (const i of list) {
+    if (i === searchElement) {
+      occurrences++;
+    }
+  }
+  return occurrences;
+};
