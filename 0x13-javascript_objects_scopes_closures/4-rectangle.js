d8693340765ec290e994ebe439819e691340175e
H-Asmaa <asmaehadar32@gmail.com>
2023-09-14 06:31:05 +0100

    Committing semistandard fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-	  if (w > 0 && h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-	  }
+    }
   }
 
   print () {
-	  let row = '';
-	  for (let i = 0; i < this.height; i++) {
+    let row = '';
+    for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-		  row += 'X';
+        row += 'X';
       }
       console.log(row);
       row = '';
-	  }
+    }
   }
 
   rotate () {
-    let tmp;
-    tmp = this.width;
+    const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
