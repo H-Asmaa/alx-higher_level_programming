ada4e63121e25f33ea49d4d90722b14d68f4d397
H-Asmaa <asmaehadar32@gmail.com>
2023-09-15 07:37:19 +0100

    Committing a script that concatenates the content of two files and saves it to an output file.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.slice(2);
+if (args.length === 3) {
+  const fileA = args[0];
+  const fileB = args[1];
+  const fileC = args[2];
+  let concate = '';
+  fs.readFile(fileA, 'utf-8', (err, data) => {
+    if (err) {
+      console.error('Error Reading from ' + fileA + ' : ' + err);
+      return;
+    }
+    concate += data;
+    concate += '\n';
+    fs.readFile(fileB, 'utf-8', (err, data) => {
+      if (err) {
+        console.error('Error Reading from ' + fileB + ' : ' + err);
+        return;
+      }
+      concate += data;
+
+      fs.writeFile(fileC, concate, 'utf-8', (err) => {
+        if (err) {
+          console.error('Erro writing to ' + fileC + ' : ' + err);
+          return;
+        }
+
+        fs.readFile(fileC, 'utf-8', (err, data) => {
+          if (err) {
+            console.error('Error Reading from ' + fileC + ' : ' + err);
+            return;
+          }
+          console.log(data);
+        });
+      });
+    });
+  });
+} else {
+  console.error('REQUIRED : files to concatenate and output file.');
+}
