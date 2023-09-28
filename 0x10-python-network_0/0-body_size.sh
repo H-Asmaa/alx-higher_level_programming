e37890bf7e2acc8c174e9cdfffa4ae57705641f4
H-Asmaa <asmaehadar32@gmail.com>
2023-09-28 13:02:37 -0700

    Committing a correction to force silence of the curl command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-LENGTH=$(curl -I "$1"| grep -i 'content-length' | awk '{print $2}')
+LENGTH=$(curl -sI "$1"| grep -i 'content-length' | awk '{print $2}')
 echo "$LENGTH"
