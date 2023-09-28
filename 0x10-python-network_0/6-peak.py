5802b74a1ff4566950e046206fd0abe68f94d91c
H-Asmaa <asmaehadar32@gmail.com>
2023-09-28 15:49:27 -0700

    Committing a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,15 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+# A script that finds the peak
+def find_peak(list_of_integers):
+    for i in range(1, len(list_of_integers) - 1):
+        prev = list_of_integers[i - 1]
+        follow = list_of_integers[i + 1]
+        curr = list_of_integers[i]
+        if curr > prev and curr > follow:
+            return curr
+    i = list_of_integers
+    if (len(i)) >= 2 and i[0] >= i[1]:
+        return i[0]
+    if (len(i)) >= 2 and i[-2] <= i[-1]:
+        return i[-1]
+    return None
