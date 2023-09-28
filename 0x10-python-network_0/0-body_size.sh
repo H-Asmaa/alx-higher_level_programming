623b4ec53d4827b9ac2aff8594b1c24ba0c14071
H-Asmaa <asmaehadar32@gmail.com>
2023-09-28 13:06:42 -0700

    Committing a script that displays the length of a content of a URL

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-LENGTH=$(curl -sI "$1"| grep -i 'content-length' | awk '{print $2}')
-echo "$LENGTH"
+curl -sI "$1"| grep -i 'content-length' | awk '{print $2}'
