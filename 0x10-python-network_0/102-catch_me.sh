b3c5035d8e266052da4e19431b7c7b2004954c0c
H-Asmaa <asmaehadar32@gmail.com>
2023-09-30 00:33:52 -0700

    Committing

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # A bash script that makes a request to 0.0.0.0:5000/catch_me.
-curl -sL -X PUT -d "You got me!" "0.0.0.0:5000/catch_me" | grep -o "You got me!" | head -n 1 | tr -d '\n'
+#curl -sL -X PUT -d "You got me!" "0.0.0.0:5000/catch_me" | grep -o "You got me!" | head -n 1 | tr -d '\n'
+#curl -sL -X PUT -d "user_id=98" "0.0.0.0:5000/catch_me" -H "Origin: HolbertonSchool"
+curl -sL -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
