04a47e8565bf28fff242ce148438464e6a920e7c
H-Asmaa <asmaehadar32@gmail.com>
2023-10-01 22:57:04 +0100

    Committing a modification for the code

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,11 @@
 """A script that takes in a URL, sends a request to the
 URL and displays the value of the X-Request-Id variable
 found in the header of the response."""
-import sys
-import urllib.request as ulib
 
 if __name__ == "__main__":
+    import sys
+    import urllib.request as ulib
+
     with ulib.urlopen(sys.argv[1]) as response:
         request_id = response.getheader("X-Request-Id")
         print("{}".format(request_id))
