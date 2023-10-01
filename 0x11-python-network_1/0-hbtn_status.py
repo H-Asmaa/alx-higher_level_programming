2e20d1b6bd348606eb137ef40c02df14b68c00a5
H-Asmaa <asmaehadar32@gmail.com>
2023-10-01 16:53:55 +0100

    Committing trial to pass another URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request as ulib
 
 if __name__ == "__main__":
-    with ulib.urlopen('https://intranet.hbtn.io/status') as response:
+    with ulib.urlopen('http://0.0.0.0:5050/status') as response:
         content = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(content)))
