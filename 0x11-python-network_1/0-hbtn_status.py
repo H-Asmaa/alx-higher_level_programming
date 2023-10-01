0aa5fc68b119b25d1b09a1b4abfad7e4a08ff268
H-Asmaa <asmaehadar32@gmail.com>
2023-10-01 15:25:29 +0100

    Committing a script that fetches info about a website

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 """A script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request as ulib
 
-content = ulib.urlopen("https://alx-intranet.hbtn.io/status")
-content_read = content.read()
-print("Body response:")
-print("\t- type: {}".format(type(content_read)))
-print("\t- content: {}".format(content_read))
-print("\t- utf8 content: {}".format(content_read.decode("utf-8")))
+if __name__ == "__main__":
+	content = ulib.urlopen("https://alx-intranet.hbtn.io/status")
+	content_read = content.read()
+	print("Body response:")
+	print("\t- type: {}".format(type(content_read)))
+	print("\t- content: {}".format(content_read))
+	print("\t- utf8 content: {}".format(content_read.decode("utf-8")))
