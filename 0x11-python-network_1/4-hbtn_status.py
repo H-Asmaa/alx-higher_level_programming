1ee2f8be6b5e3e877169d482299ace7fb2cc0abc
H-Asmaa <asmaehadar32@gmail.com>
2023-10-01 23:26:24 +0100

    Committing A script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,6 @@ if __name__ == "__main__":
     import requests
 
     response = requests.get("https://alx-intranet.hbtn.io/status")
-    print(response.text)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
