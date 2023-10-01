e249e347dcb44335c71932922d3f7f6f0cbfd094
H-Asmaa <asmaehadar32@gmail.com>
2023-10-01 22:53:45 +0100

    Committing a script that sends a POST request and displays the content correction

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,10 +2,14 @@
 """A  script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter, and displays the
 body of the response (decoded in utf-8)"""
-import urllib.request as ulib
-from sys import argv
+
 
 if __name__ == "__main__":
-    with ulib.urlopen(argv[1], data=argv[2].encode("UTF-8")) as response:
+    import urllib.request as ulib
+    from sys import argv
+
+    data = ulib.parse.urlencode({"email": argv[2]}).encode("UTF-8")
+    request = ulib.Request(argv[1], data)
+    with ulib.urlopen(request, data) as response:
         content = response.read().decode("UTF-8")
-        print(content)
+        print("{}".format(content))
