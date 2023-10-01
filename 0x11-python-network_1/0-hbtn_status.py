8af07aa292b21b47a65cab3f598ef4eb5a1981da
H-Asmaa <asmaehadar32@gmail.com>
2023-10-01 15:54:09 +0100

    Committing a script that fetches info about a website output fail correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """A script that fetches https://alx-intranet.hbtn.io/status"""
-
-import urllib.request
+import urllib.request as ulib
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode("UTF-8")))
+    with ulib.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode("UTF-8")))
