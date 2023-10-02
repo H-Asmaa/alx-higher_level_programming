ae2a259008ea4288efe9eb18fe3c83cc1212b268
H-Asmaa <asmaehadar32@gmail.com>
2023-10-02 20:53:00 +0100

    Committing script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    q = argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    try:
+        content = response.json()
+        if (content):
+            print(content.get("id"), content.get("name"))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
