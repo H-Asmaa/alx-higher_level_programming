86c2ed2e1a5f8c988de50387e750b7e7b5c8402c
H-Asmaa <asmaehadar32@gmail.com>
2023-10-02 22:18:08 +0100

    Committing a script that uses github API to display a user's id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+from sys import argv
+import requests
+
+username = argv[1]
+password = argv[2]
+url = "https://api.github.com/user"
+info = {"login": argv[1]}
+response = requests.get(url, auth=(username, password), params=info).json()
+print("{}".format(response["id"]))
