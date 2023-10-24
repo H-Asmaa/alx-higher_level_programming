c21c77cd717aee976ddc57157f1d7689282bee36
H-Asmaa <asmaehadar32@gmail.com>
2023-10-24 21:42:49 +0100

    Committing a script that sends a request to a url with an id

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*A script that sends a request to a URL with an id*/
+const request = require('request');
+const id = process.argv.slice(2);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id[0];
+request(url, (error, response, body) => {
+  if (error) {
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
