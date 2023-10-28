8bdbb22deb8bcdcdc216eb6445a0243ec16cbe83
H-Asmaa <asmaehadar32@gmail.com>
2023-10-28 17:12:28 +0100

    Committing a minimization of code and output correction

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
+/* A script that calculates the tasks completed for each user. */
 const url = process.argv.slice(2)[0];
 const request = require('request');
 request(url, (error, response, body) => {
   if (error) { return; }
-  let id = JSON.parse(body)[0].userId; let counter = 0;
+  const data = JSON.parse(body);
+  let id = null; let counter;
   const usersTasks = {};
-  for (const dict of JSON.parse(body)) {
-    if (dict.userId === id) {
-      if (dict.completed === true) { counter++; }
-    } else {
-      if (counter > 0) {
-        usersTasks[id] = counter;
-        counter = 0;
-      }
-      if (dict.completed) counter++;
+  if (!data) {
+    console.log(usersTasks);
+    return;
+  }
+  // let id = data[0].userId; let counter = 0;
+  for (const dict of data) {
+    if (dict.userId !== id) {
+      if (counter > 0) usersTasks[id] = counter;
       id = dict.userId;
+      counter = 0;
     }
+    if (dict.completed) counter++;
   }
   if (counter !== 0) usersTasks[id] = counter;
   console.log(usersTasks);
