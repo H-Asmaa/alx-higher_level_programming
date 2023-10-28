b442c57c0b20753a88196f7510d3f145b91c179a
H-Asmaa <asmaehadar32@gmail.com>
2023-10-28 16:09:25 +0100

    Committing a script that gets the content of a web page and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/* A script that gets the contents of a webpage and stores
+it in a file.*/
+const url = process.argv.slice(2)[0];
+const path = process.argv.slice(2)[1];
+const request = require('request');
+const fs = require('fs');
+request(url, (error, response, body) => {
+  if (error) {
+    return;
+  }
+  const data = body;
+  fs.writeFile(path, data, (err) => {
+    if (err) {
+      const error = {
+        Error: err,
+        errno: err.errno,
+        code: err.code,
+        syscall: err.syscall,
+        path: err.path
+      };
+      console.log(error);
+    }
+  });
+});
