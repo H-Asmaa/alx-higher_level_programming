e5129b5eab22fdd7fa789ba7b9b0f0e75a83a782
H-Asmaa <asmaehadar32@gmail.com>
2023-10-28 17:37:59 +0100

    Committing a solution using map

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,22 +4,18 @@ const url = process.argv.slice(2)[0];
 const request = require('request');
 request(url, (error, response, body) => {
   if (error) { return; }
+  let id = null; let counter = 0;
+  const map = new Map();
   const data = JSON.parse(body);
-  let id = null; let counter;
-  const usersTasks = {};
-  if (!data || !data.length) {
-    console.log(usersTasks);
-    return;
-  }
-  // let id = data[0].userId; let counter = 0;
+  if (!data || !data.length) console.log(map);
   for (const dict of data) {
     if (dict.userId !== id) {
-      if (counter > 0) usersTasks[id] = counter;
+      if (counter > 0) map.set(id, counter);
       id = dict.userId;
       counter = 0;
     }
     if (dict.completed) counter++;
   }
-  if (counter !== 0) usersTasks[id] = counter;
-  console.log(usersTasks);
+  if (counter !== 0) map.set(id, counter);
+  console.log(map);
 });
