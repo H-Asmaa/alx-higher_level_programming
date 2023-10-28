09452f1a7949f5621e19210af7279c525c56fd23
H-Asmaa <asmaehadar32@gmail.com>
2023-10-28 17:39:39 +0100

    Committing a revert to older version

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,18 +4,22 @@ const url = process.argv.slice(2)[0];
 const request = require('request');
 request(url, (error, response, body) => {
   if (error) { return; }
-  let id = null; let counter = 0;
-  const map = new Map();
   const data = JSON.parse(body);
-  if (!data || !data.length) console.log(map);
+  let id = null; let counter;
+  const usersTasks = {};
+  if (!data || !data.length) {
+    console.log(usersTasks);
+    return;
+  }
   for (const dict of data) {
     if (dict.userId !== id) {
-      if (counter > 0) map.set(id, counter);
+      if (counter > 0) usersTasks[id] = counter;
       id = dict.userId;
       counter = 0;
     }
     if (dict.completed) counter++;
   }
-  if (counter !== 0) map.set(id, counter);
-  console.log(map);
+  if (counter !== 0) usersTasks[id] = counter;
+  console.log(usersTasks);
 });
+
