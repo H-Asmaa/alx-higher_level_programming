5879c5eb2495192082131e510cdbc91a1edf180a
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 22:43:01 +0100

    Committing a script that translates the word hello to a given language.

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+#!/usr/bin/nod
+/* A script that fetches and prints how to say “Hello” depending
+on the language */
+/* global $ */
+$(document).ready(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
+  $('#btn_translate').click(function () {
+    $('#hello').empty();
+    const lang = $('#language_code').val();
+    $.get(url + lang, function (data) {
+      $('#hello').append(data.hello);
+    });
+  });
+});
