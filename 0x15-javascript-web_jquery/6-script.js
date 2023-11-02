e100011b62138aea30e748f8086a0c7cd7352746
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 09:11:11 +0100

    Committing a script that changes the text of the element header.

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/n
+/* A script that updates the text of the <header> element to New Header!!! when
+the user clicks on DIV#update_header */
+/* global $ */
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
