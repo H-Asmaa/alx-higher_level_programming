b995fba6b86f55922a9cb3f92d418bdfe6cbcd80
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 10:20:21 +0100

    Committing a script that displays the value of hello from a url fetch.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/* A script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr
+and displays the value of hello from that fetch in the HTML tag DIV#hello. */
+/* global $ */
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+$(document).ready(function () {
+  $.get(url, function (data) {
+    $('#hello').append(data.hello);
+  });
+});
