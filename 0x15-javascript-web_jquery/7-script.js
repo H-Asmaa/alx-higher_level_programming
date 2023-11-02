1d794fb8ae0a7803589dc306aa583f7fcbce8b3a
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 10:02:02 +0100

    Committing a script that fetches the character name from a URL and append it to a div.

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-#!/usr/bin/nod
+#!/usr/bin/node
 /* A script that fetches the character name from this
 URL: https://swapi-api.alx-tools.com/api/people/5/?format=json */
 const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
