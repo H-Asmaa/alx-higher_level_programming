5663e80e17217ba4a21753e80826b3f0df894bf1
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 10:12:37 +0100

    Committing a script that fetches and lists the movie titles from a url.

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/* A script that fetches and lists the title for all movies by using this
+URL: https://swapi-api.alx-tools.com/api/films/?format=json */
+/* global $ */
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$(document).ready(function () {
+  $.get(url, function (data) {
+    data.results.forEach(movie => {
+      $('UL#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
+});
