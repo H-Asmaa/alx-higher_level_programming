74324f305889fae4a6c8cb628b9b306464a7790d
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 08:55:44 +0100

    Correction of shebang

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,8 +1,8 @@
-#!/usr/bin/no
+#!/usr/bin/node
 /* A script that adds a class to an element by clique. */
 /* global $ */
 $(document).ready(function () {
-  $('#red_header').click(function () {
-    $("header").addClass('red');
+	$('#red_header').click(function () {
+	  $("header").addClass('red');
+	});
   });
-});
