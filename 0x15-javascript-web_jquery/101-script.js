fea17aef8067b97d130c1b61e7724d1d938d5c89
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 11:02:30 +0100

    Committing a script that manipulates a UL, adding li, removing li, and clearing the UL.

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/* A script that adds, removes and clears LI elements from a
+list when the user clicks: */
+/* global $ */
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').click(function () {
+    $('UL.my_list li:last').remove();
+  });
+  $('#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
