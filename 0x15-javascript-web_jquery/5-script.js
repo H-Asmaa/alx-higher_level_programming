c879e86c957e2a57ba589bf24aa06af52b98e151
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 09:03:43 +0100

    Committing a script that adds an element on clique.

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/nod
+/* A script that adds a <li> element to a list when
+the user clicks on the tag DIV#add_item. */
+/* global $ */
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+});
