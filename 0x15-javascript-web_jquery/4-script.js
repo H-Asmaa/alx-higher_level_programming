cff8ac6418f7745e895239b9bd175d3f41317a90
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 08:51:29 +0100

    A script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header.

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/* A script that toggles the class of the <header> element when
+the user clicks on the tag DIV#toggle_header. */
+/* global $ */
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red').addClass('green');
+    } else {
+      $('header').removeClass('green').addClass('red');
+    }
+  });
+});
