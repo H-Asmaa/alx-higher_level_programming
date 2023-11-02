2ef8ca18ee6535c4c099166d31c11b42ad6bce83
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 08:33:35 +0100

    Committing a script that changes the color of an element by id.

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,8 @@
-#!/usr/bin/nod
+#!/usr/bin/no
 /* A script that changes the color of an element by id. */
 /* global $ */
 $(document).ready(function () {
-  $('#red_header').css('color', '#FF0000');
+  $('#red_header').click(function () {
+    $(this).css('color', '#FF0000');
+  });
 });
