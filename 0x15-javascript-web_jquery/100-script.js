2f26b96a9d4198e365692ad11e2a767c51545f3a
H-Asmaa <asmaehadar32@gmail.com>
2023-11-02 10:49:00 +0100

    Committing a script that is included in the head and that changes the color of the header element.

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+/* A script that updates the text color of the <header> element to red (#FF0000). */
+document.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector('header');
+  element.style.color = '#FF0000';
+});
