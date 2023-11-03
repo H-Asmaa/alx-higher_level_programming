9aedc37116807530a67ae49973af8caf1f8754b3
H-Asmaa <asmaehadar32@gmail.com>
2023-11-03 02:19:38 +0100

    Committing a script that translates on a click and a keyboard click.

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+#!/usr/bin/
+/* A script that fetches and prints how to say “Hello”
+depending on the language. */
+/* global $ */
+$(document).ready(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
+  function validateInput () {
+    const lang = $('#language_code').val();
+    $.get(url + lang, function (data) {
+      $('#hello').append(data.hello);
+    });
+  }
+  $('#btn_translate').click(validateInput);
+  $('#language_code').on('keydown', function (event) {
+    if (event.keyCode === 13) {
+      validateInput();
+    }
+  });
+});
