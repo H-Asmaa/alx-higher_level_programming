c6bccac095da1171ea075d01a47b384482b09507
H-Asmaa <asmaehadar32@gmail.com>
2023-11-05 12:10:33 +0100

    Committing a script that prints all characters of a start wars movie.

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* A script that prints all characters of a Star Wars movie.*/
+const request = require('request');
+const movieId = process.argv.slice(2)[0];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+request(url, (error, response, body) => {
+  if (error) { return; }
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) { return; }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
